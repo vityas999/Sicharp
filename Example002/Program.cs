a4b746fb34eb155e81c2e1a6ddfcfc1a90a1a14c
Vitalik <vitalijzaxarov99@mail.ru>
2023-03-06 10:45:03 +0300

    Example002 User

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
