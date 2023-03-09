a7ee694240f6773195cfb468406712720dbaa1ca
Vitalik <vitalijzaxarov99@mail.ru>
2023-03-09 19:32:20 +0300

    Добавали информацию в Example010 работа с индексами

--- Example010/Program.cs
+++ Example010/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18}; 
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);// выводит номера индексов где есть число 18(find = 18)
+        break;// прекращает поиск индекса и выведет только первое найденое
+    }
+    //index = index + 1;
+    index++;
+}
