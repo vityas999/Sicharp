d861c5eebf8637cfa7a75c0add99106238829133
Vitalik <vitalijzaxarov99@mail.ru>
2023-03-09 19:18:41 +0300

    Добавали информацию в Example009 работа с массивом

--- Example009/Program.cs
+++ Example009/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+// с низу массив, индекс начинается с 0 1 2 3 4 и т.д.
+int[] array = { 12, 25, 38, 45, 56, 68, 72, 81, 744 };
+
+int result = Max(
+     Max (array[0], array[1], array[2]),
+     Max (array[3], array[4], array[5]),
+     Max (array[6], array[7], array[8])
+     );
+Console.WriteLine(result);     
+
+
+
